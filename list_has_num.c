3ba4877b1359ac0f95e81cee8d32ca2e1e73bae8
Claire Tuna <tuna.claire@gmail.com>
2013-03-13 01:17:35 -0700

    edited command line interface/added comment

--- list_has_num.c
+++ list_has_num.c
@@ -27,26 +27,33 @@ bool arrayHasNum(int numWanted, int array[], int arrayLength){
 	return false;
 }
 
+/*
+Example: command line input ./list_has_num 1 2 3 3 4 5 
+interpreted as: Does the list 2 2 3 4 5 include the number 1? 
+prints false
+*/
 int main(int argc, char** argv){
-	int numWanted = atoi(argv[1]);
-	int minIndex = 2;
-	int midIndex;
-	int midOffset;
-	int maxIndex = argc - 1;
-	int sizeOfInterval = argc - 2;
-	int pivot;
-	int myArray[sizeOfInterval];
-	int i = minIndex;
-	int j = 0;
-	while (i<=maxIndex){
-		*(myArray + j) = atoi(argv[i]);
-		i++;
-		j++;
-	}
-	if (arrayHasNum(numWanted, myArray, sizeOfInterval)){
-		printf("true\n");
-	}
-	else printf("false\n");
+	if (argc>2){
+		int numWanted = atoi(argv[1]);
+		int minIndex = 2;
+		int midIndex;
+		int midOffset;
+		int maxIndex = argc - 1;
+		int sizeOfInterval = argc - 2;
+		int pivot;
+		int myArray[sizeOfInterval];
+		int i = minIndex;
+		int j = 0;
+		while (i<=maxIndex){
+			*(myArray + j) = atoi(argv[i]);
+			i++;
+			j++;
+		}
+		if (arrayHasNum(numWanted, myArray, sizeOfInterval)){
+			printf("true\n");
+		}
+		else printf("false\n");
+		}
 	return 0;
 }
 
